d5543cf316cf7ff88da6e1c4d8373f44f65efb86
Nitin Gupta <153295329+nitingupta95@users.noreply.github.com>
2025-05-25 10:38:33 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ const corsOptions = {
   origin: 'https://second-brain-app-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'token']
 };
 
  
